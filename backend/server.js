2d9dae03160ef9c3b62b1320d7029684d4d5ef2c
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2026-07-12 18:57:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const cors = require("cors");
-
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api", productRoutes);
 
 app.get("/", (req, res) => {
-
   res.send("Backend Running");
+});
 
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP"
+  });
 });
 
 app.listen(5000, () => {
-
   console.log("Server running on port 5000");
-
 });
